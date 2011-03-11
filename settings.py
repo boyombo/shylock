a277bb0abc5d060fe4ebb757b87404cd73f0e8af
boyombo <boyombo@localhost>
2011-03-11 04:06:49 -0600

    modified settings

--- settings.py
+++ settings.py
@@ -10,10 +10,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = 'shylock'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = 'bayo_shylock'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'bayo_shylock'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'p455.p455'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
@@ -102,7 +102,7 @@ INSTALLED_APPS = (
     'expenses',
     'simplereports',
     'accounting',
-	'django_extensions',
+    #'django_extensions',
     'pagination',
 )
 
