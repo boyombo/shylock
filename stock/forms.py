af1fae6a752338e2efb21687daa848dd83082db2
bayo <bayokrapht@gmail.com>
2015-01-06 08:34:58 +0100

    added location and stock models

--- stock/forms.py
+++ stock/forms.py
@@ -1,11 +1,19 @@
 from django import forms
-from stock.models import Item, Category
+from stock.models import Item, Category, Location
+
+
+class LocationForm(forms.ModelForm):
+
+    class Meta:
+        model = Location
+
 
 class CategoryForm(forms.ModelForm):
     description = forms.CharField(widget=forms.Textarea(attrs={'rows': 5}))
 
     class Meta:
         model = Category
+
 
 class ItemForm(forms.ModelForm):
     description = forms.CharField(widget=forms.Textarea(attrs={'rows': 5}))
