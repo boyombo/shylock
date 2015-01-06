79a84d8c062d07bf38c2d05910b9d6473bb27d90
bayo <bayokrapht@gmail.com>
2015-01-06 14:13:17 +0100

    added transfer functionality

--- movement/forms.py
+++ movement/forms.py
@@ -1,28 +1,66 @@
 from django import forms
 #from movement.models import Reception, Transfer
-from movement.models import Reception, Return
-#from stock.models import LocationItems
+from movement.models import Reception, Return, Transfer
+from stock.models import Stock
 from extras.datewidget import DateTimeWidget
 from datetime import date
 
+
 class ReceptionForm(forms.ModelForm):
-    date = forms.DateField(widget=DateTimeWidget, initial=date.today, input_formats=['%d/%m/%Y'])
+    date = forms.DateField(
+        widget=DateTimeWidget, initial=date.today, input_formats=['%d/%m/%Y'])
+
     class Meta:
         model = Reception
 
+
 class ReturnForm(forms.ModelForm):
-    date = forms.DateField(widget=DateTimeWidget, initial=date.today, input_formats=['%d/%m/%Y'])
-    
+    date = forms.DateField(
+        widget=DateTimeWidget, initial=date.today, input_formats=['%d/%m/%Y'])
+
     class Meta:
         model = Return
 
-#class TransferForm(forms.ModelForm):
-#    date = forms.DateField(widget=DateTimeWidget())
-#    class Meta:
-#        model = Transfer
-#
-#    def clean(self):
-#        if 'item' in self.cleaned_data and 'quantity' in self.cleaned_data and 'from_location' in self.cleaned_data:
+
+class TransferForm(forms.ModelForm):
+    when = forms.DateField(required=False, widget=DateTimeWidget())
+
+    class Meta:
+        model = Transfer
+
+    def clean(self):
+        data = self.cleaned_data
+        if ('item' in data and 'quantity' in data
+                and 'source' in data and 'destination' in data):
+            source = self.cleaned_data['source']
+            dest = self.cleaned_data['destination']
+            item = self.cleaned_data['item']
+            qty = self.cleaned_data['quantity']
+            try:
+                source_stock = Stock.objects.get(location=source, item=item)
+            except Stock.DoesNotExist:
+                raise forms.ValidationError('No stock to transfer')
+
+            # Check quantity
+            if source_stock.quantity < qty:
+                raise forms.ValidationError(
+                    'Not enough stock. There are only %s left' %
+                    source_stock.quantity)
+
+            try:
+                dest_stock = Stock.objects.get(location=dest, item=item)
+            except Stock.DoesNotExist:
+                dest_stock = Stock.objects.create(
+                    location=dest, item=item, quantity=qty)
+
+            source_stock.quantity -= qty
+            dest_stock.quantity += qty
+            source_stock.save()
+            dest_stock.save()
+
+            return self.cleaned_data
+
+
 #            try:
 #                loc_item = LocationItems.objects.get(location=self.cleaned_data['from_location'], item=self.cleaned_data['item'])
 #            except LocationItems.DoesNotExist:
