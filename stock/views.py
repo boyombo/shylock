025454dd33526138bcc00d1ce47e9bd138c5f56a
bayo <bayokrapht@gmail.com>
2015-01-08 11:10:43 +0100

    location with new item

--- stock/views.py
+++ stock/views.py
@@ -104,6 +104,7 @@ def stock_list(request):
             template_name='stock/stock_list.html',
         )
 
+
 def newitem(request, id=None, next='stock_newitem'):
     if id:
         item = get_object_or_404(Item, pk=id)
@@ -112,13 +113,13 @@ def newitem(request, id=None, next='stock_newitem'):
     if request.method == 'POST':
         form = ItemForm(request.POST, instance=item)
         if form.is_valid():
-            item = form.save()
             messages.info(request, 'Your item has been saved')
             return redirect(next)
     else:
         form = ItemForm(instance=item)
     return render_to_response('stock/newitem.html', {'form': form},
-            context_instance=RequestContext(request))
+                              context_instance=RequestContext(request))
+
 
 def get_item(request):
     code = request.GET.get('code')
